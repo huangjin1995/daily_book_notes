3e7ace32455928c3be17560f96ee55ace0ed707c
workway <hbfeng@workway.com.cn>
2018-10-22 18:27:48 +0800

    10-22

---
+++ 数学之美/test.py
@@ -0,0 +1,8 @@
+
+# 问题：
+# 数据量太小，验证效果不可信？
+# 怎样验证？ 以下几点：
+
+https://github.com/observerss/textfilter/blob/master/keywords
+https://github.com/dongxiexidian/Chinese/tree/master/%E6%95%8F%E6%84%9F%E8%AF%8D%E5%BA%93
+
